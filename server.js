d1132bca9fa11206a7ed4f2633bb50bc0819615b
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2024-06-28 11:08:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express= require('express');
+const mongoose= require('mongoose');
+const dotenv= require('dotenv');
+var cors= require('cors');
+const app=express();
+
+
+
+
+//Add Schema 
+const students=require("./models/studSchema")
+
+//Add Router
+const router=require("./routes/router");
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+mongoose.connect(process.env.Database).then(()=>{
+    console.log("Database connected")
+}).catch((err)=>{
+    console.log("Error")
+})
+
+const PORT = 6000;
+app.listen(PORT,()=>{
+    console.log(`server start at ${PORT}`);
+})
+
+
+// mongodb+srv://admin:admin12345@cluster0.yag9vlw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
